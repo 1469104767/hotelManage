86b9cddc8a9363d89ad06258a67524d26c484c94
liyiqi888 <897757235@qq.com>
2019-12-03 14:33:06 +0800

    sql表

---
+++ WebContent/client.sql
@@ -0,0 +1,7 @@
+create table client(
+clientName varchar(10) default null,
+clientTelephone varchar(6) default null,
+clientAddress varchar(30) default null,
+clientEmail varchar(30) default null
+);
+
