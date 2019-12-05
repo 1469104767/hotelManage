29629f869f990656a3e9d363d01611d741601172
loop <1469104767@qq.com>
2019-12-05 09:56:10 +0800

    创建实体类

--- src/basics/entity/Room.java
+++ src/basics/entity/Room.java
@@ -1,5 +1,9 @@
 package basics.entity;
 
+import java.util.Date;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
 public class Room {
 	private Integer id;
 	private Integer roomNum;
@@ -8,4 +12,11 @@ public class Room {
 	private Integer roomStatus;
 	private String innerPeople;
 	private String innerTel;
+	private String idCard;
+	@JSONField(format="yyyy-MM-dd")
+	private Date startTime;
+	@JSONField(format="yyyy-MM-dd")
+	private Date endTime;
+	
+	
 }
